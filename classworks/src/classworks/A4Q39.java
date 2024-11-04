5578512e1dff0100f19d879f8227248bae83d5c5
truptiranjangantayat <truptiranjangantayat@gmail.com>
2024-11-04 09:54:29 +0530

    Update A4Q39.java

--- classworks/src/classworks/A4Q39.java
+++ classworks/src/classworks/A4Q39.java
@@ -3,7 +3,18 @@ package classworks;
 public class A4Q39 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		for (int i =1; i <= 500; i++) {
+			int sum = 0;
+			for(int j =1; j<i; j++) {
+				if(i%j == 0) {
+					sum = sum+j;
+				}
+			}
+			if(sum == i) {
+				System.out.print(i+" ");
+			}
+		}
 
 	}
 
